722d2d5c5b993f1a3262fd76b431eacd32c55849
jmarquez30 <jmarquez.30@hotmail.com>
2020-11-28 13:05:41 -0600

    import requests

--- 07_github_api.py
+++ 07_github_api.py
@@ -1,9 +1,25 @@
-import requests 
 
-#CONSTANTS
+import requests
+# CONSTANTS
 BASE_URL = 'https://api.github.com/'
-response = requests.get(BASE_URL)
-print(response.status_code)
-
-
-#username = input('Give me a github username:\t')
+# FUNCTIONS
+def get_github_user(username):
+    url = f'{BASE_URL}users/{username}'
+    respose = requests.get(url)
+    if respose.status_code == 200:
+        return respose.json()
+    return None
+def download_github_user_avatar(avatar_url, username):
+    response = requests.get(avatar_url)
+    if response.status_code == 200:
+        # download a file from internet
+        response_content = response.content
+        filename = f'tmp/{username}.png'
+        with open(filename, 'wb') as image:
+            image.write(response_content)
+            return filename
+    return None
+username = input('Give me a Github username:\t')
+user = get_github_user(username)
+filename = download_github_user_avatar(user['avatar_url'], username)
+print(filename)
