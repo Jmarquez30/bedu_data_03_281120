46752e366213b9138749b27b627647bc2dd9af69
jmarquez30 <jmarquez.30@hotmail.com>
2020-11-28 10:25:01 -0600

    IVA

--- 02_pozoleria.py
+++ 02_pozoleria.py
@@ -4,5 +4,6 @@ precios_sin_iva = [415, 90, 355, 385, 115, 100, 250, 600]
 
 for precio in precios_sin_iva:
     resultado = precio * (1 + IVA)
-    print(f'{precio} con IVA incluido es: $', round(resultado, 2))
-    
+    resultado = round(resultado, 2)
+    resultado_impreso = f'${precio} con IVA incluido: ${resultado}'
+    print(resultado_impreso)
